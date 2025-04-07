f66a21dcc32ab212bd2d94b25176844aa19d7950
Felipe Guarda <felipe.guarda@proton.me>
2025-04-06 22:42:44 -0300

    Update banco.sql por feblanco

--- banco.sql
+++ banco.sql
@@ -2,6 +2,8 @@ CREATE TABLE dados (
     AlunoID int,
     Nome varchar(50),
     Sobrenome varchar(50),
+    Sexo varchar(50),
+    Idade varchar int,
     Endereco varchar(150),
     Cidade varchar(50),
     Host varchar(50)
